ec0efd6ffc96170a958efee22bf807361558fc4c
Adrien Herman <herman1.adrien@gmail.com>
2021-06-17 20:08:57 +0200

    Delete geolocalisation.js

--- geolocalisation.js
+++
@@ -1,5 +0,0 @@
-function geolocalisation() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude, position.coords.longitude);
-    });
-}
